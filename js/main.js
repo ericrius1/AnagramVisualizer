489a04a303e38765eabed26efdcbb8014cccde73
Eric <Ericrius1@gmail.com>
2013-07-09 09:15:45 -0700

    staggering words

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
+/**
+ * Given an input string, write a function that produces all possible anagrams
+ * of a string and outputs them as an array. Don't worry about duplicate
+ * strings. What is the time complexity of your solution?
+ *
+ * Extra credit: Return only unique strings without using _.uniq().
+ */
 
 var allAnagrams = function(string) {
   var result = {};
-  (function makeWord (word, remaining){
-    if(!remaining){
-      console.log(word)
+  (function makeWord(word, remaining) {
+    if (!remaining) {
       return result[word] = true;
-   }
-      for(var i = 0; i < remaining.length; i++){
-        makeWord(word + remaining[i], remaining.substr(0, i) + remaining.substr(i+1));
-      }
-    })('', string);
-    return Object.keys(result);
-  };
+    }
+    for (var i = 0; i < remaining.length; i++) {
+      makeWord(word + remaining[i], remaining.substr(0, i) + remaining.substr(i + 1));
+    }
+  })('', string);
+  return Object.keys(result);
+};
 
 
 
-var allAnagramsArray = allAnagrams('love');
-console.log(allAnagramsArray);
+var allAnagramsArray = allAnagrams('herb');
+
+for (var i = 0; i < allAnagramsArray.length; i++) {
+  setTimeout(function(y) {
+    console.log(allAnagramsArray[y]);
+  }, i * 1000, i);
+}
